d374bdae67d6b52679a1d4f6b8da92ae2c990cf7
Alexander Vandenberg-Rodes <alexvr@gmail.com>
2017-02-14 12:38:57 -0800

    combined simulation and initial binning code

--- R/sim_lag_coalescent.R
+++ R/sim_lag_coalescent.R
@@ -4,9 +4,8 @@
 #' 
 #' @export
 #' 
-#' @param lag The 'true' lag parameter, indicating how much the effective population size
-#'  N_e(t) is shifted to the right relative to the sampling rate f(t).
-#' relative to the last series.
+#' @param lag The 'true' lag parameter, indicating how far the sampling rate f(t) is shifted
+#' forwards in time relative to effective population size N_e(t).
 #' @param type Which type of latent trajectory to use. Either 'cyclic' or 'boombust'.
 #' @param c Constant parameter in equation \eqn{f(t) = c N_e(t)^\beta}.
 #' @param beta Power parameter in above equation relating sampling rate f(t) to 
@@ -24,7 +23,7 @@ sim_lag_coalescent <- function(lag=0, type='cyclic', c=1, beta=2, scaling=.05) {
   else stop("unrecognized trajectory type")
   
   # lagged sampling rate function
-  samp_fun <- function(x) coal_fun(x - lag)
+  samp_fun <- function(x) coal_fun(x + lag)
   
   # generate sampling times from fun
   sampling_times <- pref_sample(samp_fun, lim=c(0, 40), c=c, beta=beta, upper=NULL, grid.len=2000)
@@ -35,6 +34,108 @@ sim_lag_coalescent <- function(lag=0, type='cyclic', c=1, beta=2, scaling=.05) {
   sim <- coalsim(samp_times=sampling_times, n_sampled=n_samples, traj=coal_fun)
   events = sim[c('coal_times', 'samp_times', 'n_sampled')]
   
-  return(list(events=events, lag=lag, type=type,
+  return(list(coal_data=coal_data, lag=lag, type=type,
               rate_functions=list(coal_fun=coal_fun, samp_fun=samp_fun)))
+}
+
+
+#' Approximation of Coalescent likelihood as a binned Poisson process
+#'
+#' @param coal_data List containing attributes coalescent times, sampling times, number of lineages,
+#' and number of grid points.
+#'
+bin_coal <- function(coal_data, n_bins, st=NULL, fin=NULL) {
+
+  # extract out data
+  samp_times <- coal_data$samp_times
+  coal_times <- coal_data$coal_times
+  n_sampled <- coal_data$n_sampled
+
+  # create grid of times
+  all_times <- sort(c(samp_times, coal_times));
+  grid <- seq(0, max(all_times), length.out = n_bins)
+  midpts <- grid[-1] - diff(grid)/2
+
+  # create coalescent likelihood for f_1 -- log effective pop size
+  init <- coal_lik_init(samp_times=samp_times, n_sampled=n_sampled,
+                        coal_times=coal_times, grid=grid)
+
+  # Transform 'init' so that Coalescent(f_1) is Poisson with offset E
+  # likelihood: Coalescent(f_1) = sum(y .* (-f_1) - E .* exp(-f_1))
+  ind <- 1:(init$ng)
+
+  # Create factors -- one per bin
+  ind_rep = rep(ind, init$gridrep)
+
+  # Coalescence over [t, t+delta] occurs at rate delta * l(l-1)/2, where
+  # l = number of active lineages at time t.
+  E_expanded = init$D * init$C
+
+  # calculates the offset for each bin.
+  E <- as.vector(tapply(E_expanded, ind_rep, sum))
+
+  # There is a classic error that fails for zero elements of E,
+  # so set those small values
+  E <- ifelse(E==0, 1, E)
+
+  # Number of coalescence events in each bin
+  y <- as.vector(tapply(init$y, ind_rep, sum))
+
+  # Calculate number of sampling events in each bin
+  new_samp_times <- rep(0,length(grid)-1)
+  for (j in 2:length(grid)){
+    new_samp_times[(j-1)] <- sum(n_sampled[samp_times > grid[j-1] &
+                                             samp_times <= grid[j]])
+  }
+  # add sampling time at boundary (t=0).
+  new_samp_times[1] <- new_samp_times[1]+1
+
+  # treat the counts (null) beyond the oldest sampling time as missing data
+  miss <- sum(grid > max(samp_times))
+
+  # mask for available sampling times data
+  Nvis_samp <- init$ng - miss + 1
+
+  # Grid size
+  delta <- diff(grid)[1]
+
+  # extend grid if necessary
+  ext = extend_grid(st, fin, grid)
+
+  # Compute the pairs of Poisson observations over grid points
+  counts <- matrix(-1, ext$n_grid, 2)
+  counts[ext$old_idx, 1] <- y
+  counts[ext$old_idx[1:Nvis_samp], 2] <- new_samp_times[1:Nvis_samp]
+
+  offset <- matrix(-1, ext$n_grid, 2)
+  offset[ext$old_idx, 1] <- E
+  offset[ext$old_idx[1:Nvis_samp], 2] <- delta
+
+  poisson_args <- list(counts=counts, breaks=ext$new_grid, offset=offset, delta=delta, N=ext$n_grid, n_series=2)
+
+  return(poisson_args)
+}
+
+
+#' Extend existing grid, keeping grid width constant.
+#'
+#' @param st Maximum start time of new grid
+#' @param fin Minimum finish time of new grid
+#' @param grid Existing grid to extend
+extend_grid <- function(st, fin, grid) {
+  dt = diff(grid)[1]
+  if (is.null(st) || st == grid[1]) left <- c()
+  else {
+    stopifnot(st < 0)
+    left = -rev(1 : ceiling(-st / dt)) * dt
+  }
+
+  if (is.null(fin) || fin == grid[length(grid)]) right <- c()
+  else {
+    stopifnot(fin > max(grid))
+    right <- dt * (1 : ceiling(fin - max(grid)) / 2) + max(grid)
+  }
+  new_grid <- c(left, grid, right)
+  idx <- (1 : (length(grid) - 1)) + length(left)
+  return(list(left=left, right=right, new_grid=new_grid, old_idx=idx, n_grid=length(new_grid) - 1))
 }
--- R/sim_lag_poisson.R
+++ R/sim_lag_poisson.R
@@ -1,25 +1,27 @@
 #' Generate a realization of log Gaussian Cox process, with each marginal latent function a shifted copy of the last.
 #' 
-#' @export
 #' @param n_series Number of correlated series.
-#' @param lag The 'true' lag parameter, indicating how much each series is shifted to the right relative to the last series. Must be a vector of length=n_series, with the last element set to zero.
+#' @param lag The 'true' lag parameter, indicating how much each series is shifted to the right relative to the first series. Must be a vector of length = (n_series-1).
 #' @param sim_res Resolution (bin-width) used to generate the time stamps.
 #' @param type Which type of latent function to use. Either 'cyclic' or 'boombust'.
 #' @param scaling Constant scaling factor for the overall Poisson rate.
 #'
-gen_poisson_fixed <- function(n_series = 3, lag = c(0.5, -1.2, 0.), sim_res=0.01, type='cyclic', scaling=.1) {
+#' @export
+sim_lag_poisson <- function(n_series = 2, lag = 0.5, sim_res=0.01, type='cyclic', scaling=.1) {
   # Generate realization of lagged poisson point processes.
   
-  if (length(lag) != n_series)
+  if (length(lag) != (n_series - 1))
     stop("length(lag) != n_series")
-  if (lag[n_series] != 0.)
-    warning("last lag should be zero")
   if (type == 'cyclic')
     fun <- function(t) phylodyn::cyclic_traj(t)
   else if (type == 'boombust')
     fun <- function(t) phylodyn::boombust_traj(t, bust=15)
   else stop("unrecognized trajectory type")
   
+  # extend lag vector to include first series
+  lag = c(0, lag)
+
+  # Set up grid for simulating the Poisson processes
   t <- seq(-40, 40, by=sim_res)
   dt <- t[2] - t[1]
   rates <- fun(t) * dt * scaling
@@ -29,7 +31,7 @@ gen_poisson_fixed <- function(n_series = 3, lag = c(0.5, -1.2, 0.), sim_res=0.01
   x <- c()
   for (k in 1:n_series) {
     y <- rpois(length(rates), rates)
-    all_events <- t[y>0] - lag[k]
+    all_events <- t[y>0] + lag[k]
     events[[k]] <- all_events[(all_events > st) & (all_events < fin)]
     x <- cbind(x, rates[(t > (st - lag[k])) & (t < (fin - lag[k]))])
     
@@ -41,11 +43,12 @@ gen_poisson_fixed <- function(n_series = 3, lag = c(0.5, -1.2, 0.), sim_res=0.01
 #' 
 #' @export
 #' @param times List of vectors of timestamps.
-#' @param n_bins number of equally-spaced bins to use.
+#' @param n_bins Number of equally-spaced bins to use.
+#' @param grid Existing grid to use.
 #' @param st Assumed start time of data collection. Default = min(times) - 0.1 * bin_width
 #' @param fin Assumed end time of data collection. Default = max(times) + 0.1 * bin_width
 #'
-bin_poisson <- function(times, n_bins=100, bins=NULL, st=NULL, fin=NULL) {
+bin_poisson <- function(times, n_bins=100, grid=NULL, st=NULL, fin=NULL) {
   # Input: list of vectors, one per timepoint
   if (is.numeric(times)) {
     times <- list(y=times)
@@ -53,21 +56,20 @@ bin_poisson <- function(times, n_bins=100, bins=NULL, st=NULL, fin=NULL) {
   n_series <- length(times)
   
   # Find first and last point
-  if (is.null(bins)) {
-    bins <- make_grid(times, n_bins, st=st, fin=fin)
+  if (is.null(grid)) {
+    grid <- make_grid(times, n_bins, st=st, fin=fin)
   } else {
-    n_bins <- length(bins) - 1
+    n_bins <- length(grid) - 1
   }
-  dt <- bins[2] - bins[1]
+  dt <- grid[2] - grid[1]
   offset <- matrix(dt, n_bins, n_series)
   counts <- matrix(0, n_bins, n_series)
   for (k in 1:n_series) {
     y <- times[[k]]
-    counts[,k] <- hist(y, breaks=bins, plot=FALSE)$counts
+    counts[,k] <- hist(y, breaks=grid, plot=FALSE)$counts
   }
-  return(list(counts=counts, breaks=bins, offset=offset, delta=dt, N=n_bins, 
-              N_vis = rep(n_bins, n_series, labels=names(times)), n_series=n_series,
-              coalescent = 0))
+  return(list(counts=counts, breaks=grid, offset=offset, delta=dt, N=n_bins, 
+              N_vis = n_bins * n_series, n_series=n_series))
 }
 
 make_grid <- function(times, n_bins, st, fin) {
